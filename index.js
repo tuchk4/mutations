e61b6a8244f61b00e2508809210af4493dbc5763
Valeriy <valeriy.sorokobatko@gmail.com>
2014-07-31 09:51:38 +0300

    index

--- index.js
+++ index.js
@@ -2,33 +2,22 @@
 
 var transform = require('./src/index');
 
-// as arr
-var result = transform([{
-  id: 1,
-  name: 'Valeriy',
-  key: 'Efef14#41*31#$%w'
-}], {
-  type: 'select',
-  fields: {
-    name: {
-      rename: 'my.info.name'
-    },
-    key: {
-      encode: function(value){
-        return '!!@##' + '____' + value;
-      }
-    }
-  }
-});
 
 // as obj
 var result = transform({
   id: 1,
   name: 'Valeriy',
-  key: 'Efef14#41*31#$%w'
+  key: 'Efef14#41*31#$%w',
+  test: {
+    info: {
+      state: 'Kh'
+    }
+  }
 }, {
-  type: 'select',
   fields: {
+    'test.info.state': {
+      rename: 'state'
+    },
     name: {
       rename: 'my.info.name'
     },
