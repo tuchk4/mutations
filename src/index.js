0992894fa36ee7902780eda397b47bdf2904d318
Valeriy <valeriy.sorokobatko@gmail.com>
2014-07-31 09:50:03 +0300

    added support of chain keys for rule

--- src/index.js
+++ src/index.js
@@ -34,36 +34,92 @@ function selectParam(key, value, rule) {
   };
 }
 
-function insert(object, key, value){
+function insert(obj, key, value){
   var parts = key.split('.'),
     last = parts.pop();
 
 
   for (var i = 0, size = parts.length; i < size; i++){
-    if (object[parts[i]] === undefined){
-      object[parts[i]] = {};
+    if (obj[parts[i]] === undefined){
+      obj[parts[i]] = {};
     }
 
-    object = object[parts[i]];
+    obj = obj[parts[i]];
   }
 
-  if (!Utils.types.isObject(object)){
-    throw new Error('Invalid rename property or last obj element is not Array');
+  if (!Utils.types.isObject(obj)){
+    throw new Error('Invalid rename property or last obj element is not obj or arr');
   }
 
 
-  object[last] = value;
+  obj[last] = value;
+}
+
+
+function remove(obj, path){
+  var dots = path.split('.'),
+    last = dots.pop(),
+    isExist = true;
+
+  for (var i = 0, size = dots.length; i < size; i++){
+    if (obj.hasOwnProperty(dots[i])) {
+      if (obj[dots[i]] != undefined) {
+        obj = obj[dots[i]];
+      } else {
+        isExist = false;
+      }
+    } else {
+      isExist = false;
+    }
+  }
+
+  if (isExist){
+    delete obj[last];
+  }
+}
+
+function get(obj, path){
+  var dots = path.split('.'),
+    current = obj;
+
+  for (var i = 0, size = dots.length; i < size; ++i) {
+    if (current.hasOwnProperty(dots[i])) {
+      if(current[dots[i]] === undefined) {
+        return undefined;
+      } else {
+        current = current[dots[i]];
+      }
+    }
+  }
+  return current;
 }
 
 function resolve(object, rule) {
   var resolver = Types[rule.type];
 
   var transformed = {};
+
   for (var key in object){
     if (object.hasOwnProperty(key) && !resolver.isExcluded(key, rule) && !isExcluded(key, rule)){
       var param = selectParam(key, object[key], rule);
-
       insert(transformed, param.key, param.value);
+    }
+  }
+
+  for (var path in rule.fields){
+    if (rule.fields.hasOwnProperty(path)){
+      var dots = path.split('.');
+
+      if (dots.length > 1){
+        var value = get(object, path),
+          param = selectParam(path, value, rule);
+
+        if (path != param.key) {
+          remove(transformed, path);
+        }
+
+        insert(transformed, param.key, param.value);
+      }
     }
   }
 
